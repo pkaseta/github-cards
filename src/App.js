f45941d9cb9f6d8a7ffbca902b19f5c133f3f956
pkaseta <kasetapaul@gmail.com>
2021-04-11 23:21:30 -0400

    completed functionality and styling for github cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import { Button, Card, Col, InputGroup, FormControl } from 'react-bootstrap'
+import Form from 'react-bootstrap/Form'
 
 function App() {
+  const [user, setUser] = useState({})
+  const [active, setActive] = useState(false)
+
+  const handleToggle = () => {
+
+    console.log('Button is working')
+    fetch('https://api.github.com/users/pkaseta ')
+      .then((res) => res.json())
+      .then((data) => setUser(data))
+      .then(setActive(!active))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mainPage'>
+
+      <h1>Paul K's Github Card</h1>
+      <br />
+
+      <Button style={{ marginBottom: '20px' }} variant="outline-dark" onClick={handleToggle}>
+        {active === false ? 'View Pauls Github' : 'cancel view'}</Button>
+
+      {active === true ? <Card style={{ width: '18rem', margin: 'auto' }}>
+        {active === true ? <Card.Img variant="top" src={user.avatar_url} /> : ''}
+        <Card.Body>
+          <Card.Title><h2>{user.name}</h2></Card.Title>
+          <hr />
+          <Card.Text>
+            <h4>Type</h4>
+            {user.type}
+          </Card.Text>
+          <Card.Text>
+            <h4>Username</h4>
+            {user.login}
+          </Card.Text>
+          <Card.Text>
+            <h4>Repos</h4>
+            {user.public_repos}
+          </Card.Text>
+          <Card.Text>
+            <h4>Biography</h4>
+            {user.bio}
+          </Card.Text>
+        </Card.Body>
+      </Card> : <div></div>
+      }
     </div>
   );
 }
